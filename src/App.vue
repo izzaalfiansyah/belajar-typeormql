167e98e4886237f9a5869af0377216c1cebed03b
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2024-10-21 22:24:51 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const name = ref("World");
+</script>
+
+<template>
+  <v-app>
+    <center>
+      <v-text-field v-model="name" label="Name"></v-text-field>
+      Hello {{ name }}!
+    </center>
+  </v-app>
+</template>
